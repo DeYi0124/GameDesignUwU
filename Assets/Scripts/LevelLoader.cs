2d4bc547c2bc4f2e7bd2d540002e0c4cfa02264d
TheCrack <thecrack@LAPTOP-2G6UD9U4>
2024-10-09 22:22:35 +0800

    added faded animation

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    void Awake()
+    {
+        CurrentBike[] currs = FindObjectsOfType<CurrentBike>();
+        foreach (CurrentBike curr in currs) {
+            curr.OnSave += OnSave;
+        }
+    }
+
+    void OnSave(CurrentBike currBike) {
+        //Debug.Log("UWUevent!");
+        LoadingScreen();
+    }
+
+    void LoadingScreen() {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel() {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

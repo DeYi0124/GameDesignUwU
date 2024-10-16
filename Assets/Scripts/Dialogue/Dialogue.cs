2a5c45ad8ba9777084e7601b572f3da1807b8e8d
Photatoeggs <tomo032192@gmail.com>
2024-10-17 02:44:26 +0800

    Content of Dialogue can be from .txt

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Dialogue
 {   
     public string name;
-    public string[] sentences;
+    [TextArea(3,10)]
+    public List<string> sentences;
 
 }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public int id;
     public Dialogue dialogue;
 
+    List<string> ReadDialogueFile(int id){
+        string path = Application.streamingAssetsPath + "/Dialogue/" + id.ToString() + ".txt";
+        List<string> DialogueContent = File.ReadAllLines(path).ToList();
+        return DialogueContent;
+    }
     public void TriggerDialogue(){
+        List<string> dialogueContent = ReadDialogueFile(id);
+        dialogue.name = dialogueContent[0]; 
+        for( int i = 1;i < dialogueContent.Count; i++){
+            dialogue.sentences.Add(dialogueContent[i]);
+        }
+
+        
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 }

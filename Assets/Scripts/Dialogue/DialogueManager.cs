2b32be28f791c4deee2805b591ac436047d50c2b
Photatoeggs <tomo032192@gmail.com>
2024-10-22 17:34:39 +0800

    added dialogue character

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -10,46 +10,55 @@ public class DialogueManager : MonoBehaviour
 
     public Text nameText;
     public Text dialogueText;
-    public Animator animator;
+    public Animator dialogueBoxAnimator;
+    public Animator character1Animator;
     private Queue<string> sentences;
+    private Queue<string> names;
     public int id;
 
     // Start is called before the first frame update
     void Start()
     {  
-        Debug.Log(id);
-        GameObject BackGround = GameObject.Find("BackGround");
-        SpriteRenderer renderer = BackGround.GetComponent<SpriteRenderer>();
-        renderer.sprite = LoadImageFile(id);
-        List<Vector3> ScaleList = new List<Vector3>();
-        ScaleList.Add(new Vector3(2,1,0));
-        ScaleList.Add(new Vector3(2,1,0)); 
-        RectTransform transform = BackGround.GetComponent<RectTransform>();
-        transform.localScale = ScaleList[id-1];
+        LoadBackGround();
+        List<int> charNum = new List<int>();
+        charNum.Add(2);
+        charNum.Add(2);
+        LoadCharacters(charNum[id-1]);
         sentences = new Queue<string>();
-        animator.SetBool("IsOpen", false);
+        names = new Queue<string>();
+        dialogueBoxAnimator.SetBool("IsOpen", false);
+        character1Animator.SetBool("InScene",false);
     }
 
 
     public void StartDialogue (Dialogue dialogue){
         //Debug.Log("starting conversation with "+ dialogue.name);
-        animator.SetBool("IsOpen", true);
-        nameText.text = dialogue.name;
-        
+        dialogueBoxAnimator.SetBool("IsOpen", true);
+        character1Animator.SetBool("InScene",true);
+        names.Clear();
         sentences.Clear();
-
-        foreach(string sentence in dialogue.sentences){
-            sentences.Enqueue(sentence);
+        bool isName = true;
+        foreach(string sentence in dialogue.content){
+            //Debug.Log(sentence);
+            if(isName){
+                names.Enqueue(sentence);
+                isName = false;
+            }
+            else{
+                sentences.Enqueue(sentence);
+                isName = true;
+            }
         }
 
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence(){
-        if (sentences.Count == 0){
+    public void DisplayNextSentence(){ 
+        if (sentences.Count == 0 || names.Count == 0){
             EndDialogue();
             return;
         }
+        nameText.text = names.Dequeue();
         string sentence = sentences.Dequeue();
         //Debug.Log(sentence);
         //dialogueText.text = sentence;
@@ -57,7 +66,6 @@ public class DialogueManager : MonoBehaviour
         StartCoroutine(TypeSentence(sentence));
 
     }
-
     IEnumerator TypeSentence (string sentence){
         dialogueText.text = "";
         foreach(char letter in sentence.ToCharArray()){
@@ -73,19 +81,53 @@ public class DialogueManager : MonoBehaviour
     }
     
     void EndDialogue(){
-        animator.SetBool("IsOpen", false);
+        dialogueBoxAnimator.SetBool("IsOpen", false);
         //Debug.Log("end of conversation.");
     }
     
-    private Sprite LoadImageFile(int id){
-        string path = Application.streamingAssetsPath + "/BackGround/" + id.ToString() + ".png";
+    private void LoadBackGround(){
+        GameObject BackGround = GameObject.Find("BackGround");
+        SpriteRenderer bgRenderer = BackGround.GetComponent<SpriteRenderer>();
+        string bgPath = Application.streamingAssetsPath + "/BackGround/" + id.ToString() + ".png";
+        bgRenderer.sprite = LoadImageFile(bgPath);
+        List<Vector3> ScaleList = new List<Vector3>();
+        ScaleList.Add(new Vector3(2,1,0));
+        ScaleList.Add(new Vector3(8,8,0)); 
+        RectTransform transform = BackGround.GetComponent<RectTransform>();
+        transform.localScale = ScaleList[id-1];
+    }
+    private void LoadCharacters(int num){
+        for (int i = 1; i<= num; i++){
+            LoadCharacter(i);
+        }
+    }
+    private void LoadCharacter(int characterID){
+        string characterObject = "character" + characterID.ToString();
+        GameObject Character = GameObject.Find(characterObject);
+        SpriteRenderer charRenderer = Character.GetComponent<SpriteRenderer>();
+        string charPath = Application.streamingAssetsPath + "/Character/" + id.ToString() +'/'+ characterID.ToString() + ".png";
+        charRenderer.sprite = LoadImageFile(charPath);
+        List<List<Vector3>> ScaleList = new List<List<Vector3>>();
+        ScaleList.Add(new List<Vector3>());
+        ScaleList[0].Add(new Vector3(0,0,0));
+        ScaleList[0].Add(new Vector3(0,0,0));
+        ScaleList.Add(new List<Vector3>());
+        ScaleList[1].Add(new Vector3(15,15,0));
+        ScaleList[1].Add(new Vector3(15,15,0));
+
+        RectTransform transform = Character.GetComponent<RectTransform>();
+        transform.localScale = ScaleList[id-1][characterID-1];
+
+    }
+
+    private Sprite LoadImageFile(string path){
         byte[] imgData;
         Texture2D tex = new Texture2D(2, 2);
         imgData = File.ReadAllBytes(path);
         tex.LoadImage(imgData);
         Vector2 pivot = new Vector2(0.5f, 0.5f);
         Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), pivot, 100.0f);
-        Debug.Log(id.ToString()+"sprite");
+        //Debug.Log(id.ToString()+"sprite");
         return sprite;
 
 

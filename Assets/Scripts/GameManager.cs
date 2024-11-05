5661620a95f15107bbeede2d3302d069067e8965
Photatoeggs <tomo032192@gmail.com>
2024-11-05 16:02:37 +0800

    fixed merge conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -105,11 +105,8 @@ public class GameManager : MonoBehaviour
 
     void UpdateTime() {
         if(!pause) {
-<<<<<<< HEAD
             time += TimeScale;
-=======
-            time += 1;
->>>>>>> origin/main
+
             // //Debug.Log(time);
             // int tmpEnt = Random.Range(1, maxEnt+1);
             // if(tmpEnt <= 50) {

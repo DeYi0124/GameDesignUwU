0f456eda8c9ab03184da35e11e7e6bbc62562c8e
Photatoeggs <tomo032192@gmail.com>
2024-11-07 00:39:51 +0800

    added enemy animation

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -5,30 +5,41 @@ using UnityEngine.UI;
 public class AIController : MonoBehaviour
 {
 
-    int MoveSpeed = 4;
-    int MaxDist = 10;
-    int MinDist = 2;
+    public Animator enemyAnimator;
+    private int MoveSpeed = 4;
+    private int MaxDist = 10;
+    private int MinDist = 2;
+
     void Start()
     {
-
+        enemyAnimator.SetBool("isWalking", false);
     }
 
     void Update()
     {
+        
         // transform.LookAt(Player);
-        transform.right = CarController.Instance.transform.position - transform.position;
-
+        Vector3 positionDiff = CarController.Instance.transform.position - transform.position;
+        bool flipped = false;
+        var enemySprite = GameObject.FindWithTag("EnemySprite").GetComponent<SpriteRenderer>();
+        if(positionDiff.x == 0)
+            return;
+        enemySprite.flipY = (positionDiff.x < 0);
+        transform.right = positionDiff;
+        enemyAnimator.SetBool("isSprinting",false);
+        enemyAnimator.SetBool("isWalking", false);
         if (Vector3.Distance(transform.position, CarController.Instance.transform.position) >= MinDist)
         {
-
-            MoveSpeed = 4;
+            enemyAnimator.SetBool("isSprinting",false);
+            enemyAnimator.SetBool("isWalking",true);
+            MoveSpeed = 1;
             transform.position += transform.right * MoveSpeed * Time.deltaTime;
             if (Vector3.Distance(transform.position, CarController.Instance.transform.position) <= MaxDist)
             {
-                MoveSpeed = 8;// - Vector3.Distance(transform.position, CarController.Instance.transform.position);
+                enemyAnimator.SetBool("isSprinting",true);
+                MoveSpeed = 2;// - Vector3.Distance(transform.position, CarController.Instance.transform.position);
                 transform.position += transform.right * MoveSpeed * Time.deltaTime;
-
-                //Here Call any function U want Like Shoot at here or something
+                    //Here Call any function U want Like Shoot at here or something
             }
 
         }

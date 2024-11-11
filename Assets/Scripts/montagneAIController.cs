a062d30267cc2dea0b227810d559c3f677ee06cb
Photatoeggs <tomo032192@gmail.com>
2024-11-11 15:15:59 +0800

    added blocking enemy

---
+++ Assets/Scripts/montagneAIController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class montagneAIController : MonoBehaviour
+{
+    public Animator enemyAnimator;
+    private int MoveSpeed = 4;
+    private int outerDist = 15;
+    private int innerDist = 13;
+    private bool inside = false;
+    void Start()
+    {
+        enemyAnimator.SetBool("isWalking", false);
+    }
+
+    void Update()
+    {
+        
+        // transform.LookAt(Player);
+        Vector3 positionDiff = CarController.Instance.transform.position - transform.position;
+        var enemySprite = GameObject.FindWithTag("EnemySprite").GetComponent<SpriteRenderer>();
+        if(positionDiff.x == 0)
+            return;
+        enemySprite.flipY = (positionDiff.x < 0);
+        transform.right = positionDiff;
+
+        if (Vector3.Distance(transform.position, CarController.Instance.transform.position) >= outerDist)
+        {
+            enemyAnimator.SetBool("isWalking",true);
+            MoveSpeed = 1;
+            transform.position += transform.right * MoveSpeed * Time.deltaTime;
+        }
+        else{
+            enemyAnimator.SetBool("isWalking",false);
+            enemyAnimator.SetBool("isSpinning",true);
+        }
+
+
+    }
+}

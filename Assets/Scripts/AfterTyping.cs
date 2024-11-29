6708d64bb578f7d93440f0d4e05f55c299494450
Photatoeggs <tomo032192@gmail.com>
2024-11-30 01:40:26 +0800

    you're goddamn right

--- Assets/Scripts/AfterTyping.cs
+++ Assets/Scripts/AfterTyping.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class AfterTyping : MonoBehaviour
 {
@@ -10,17 +11,34 @@ public class AfterTyping : MonoBehaviour
     public float speed = 0.1f;
     public string fullText;
     private string currentText = "";
+    static public string playerName = "";
+    private bool submitted = false;
     public TextMeshProUGUI thing;
-    public GameObject thing2;
+    public GameObject inputField;
+    public TextMeshProUGUI inputText;
+    public GameObject submitButton;
+    public GameObject whatsMyPurpose;
+    public AudioSource audioSource;
+    public AudioSource epicAFBGM;
     void Start()
     {
         StartCoroutine(ShowText());
+        submitted = false;
     }
+    void Awake(){
 
+
+    }
+    void Update(){
+        if(submitted){
+            inputText.color = Color.red;
+        }
+    }
     IEnumerator ShowText()
     {
         yield return new WaitForSeconds(delay);
-        thing2.SetActive(true);
+        inputField.SetActive(true);
+        submitButton.SetActive(true);
         this.GetComponent<Animator>().enabled = true;
         for (int i = 0; i < fullText.Length; i++)
         {
@@ -36,7 +54,21 @@ public class AfterTyping : MonoBehaviour
             yield return new WaitForSeconds(speed);
         }
     }
-
+    public void sumbit(){
+        submitted = true;
+        whatsMyPurpose.GetComponent<whatsMyPurpose>().readPlayerNameFromScript();
+        audioSource.Play();
+    }
+    public void readName(string input){
+        playerName = input;
+    }
+    public string getPlayerName() {
+        return playerName;
+    }
+    IEnumerator changeScene(){
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MainMenu");
+    }
 }
     
 
---
+++ Assets/Scripts/whatsMyPurpose.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System.IO;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class whatsMyPurpose : MonoBehaviour
+{
+    public static whatsMyPurpose Instance;
+    private string playerName = "";
+    // Start is called before the first frame update
+    void Awake(){
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+        }
+        else
+            Instance = this;
+        
+        if(FindObjectsOfType<CarController>().Length > 1) {
+            Debug.Log("DestroyinG" + FindObjectsOfType<CarController>().Length.ToString());
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        } else
+            DontDestroyOnLoad(gameObject);
+    }
+    public void readPlayerNameFromScript(){
+        playerName = AfterTyping.playerName;
+        Debug.Log(playerName);
+    }
+    public string getPlayerName(){
+        return playerName;
+    }
+
+
+
+}
+
